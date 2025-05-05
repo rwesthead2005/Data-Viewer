276c1367c3cea7888d1c621cf27bfca18f02a93b
Alfred Jijo <alfredjijo28@gmail.com>
2025-05-05 21:49:04 +0100

    add opening of csvfile

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <typedef.h>
 #include <stb_ds.h>
+
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(void) {
 	std::cout << "Hello, World!" << std::endl;
+	std::string line, value;
+	std::vector<std::vector<std::string>> table;
+	std::ifstream file("data.csv");
+
+	if (!file.is_open()) {
+		std::cout << "Error opening file" << std::endl;
+		return 1;
+	}
+
+	file.close();
 	return 0;
 }
